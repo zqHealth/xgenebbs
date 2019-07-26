06bf76dbbbb0868e8bf744209f1ed1ac4922bd24
vagrant <vagrant@homestead>
2019-07-26 06:03:08 +0000

    layout of basic pages

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,11 @@
+
+<footer class="footer">
+    <div class="container">
+        <p class="float-left">
+            Copyright ©️ X-Gene. All Rights Reserved.
+        </p>
+        <p class="float-right">
+            <a href="mailto:zhangqing_health@outlook.com">联系我们</a>
+        </p>
+    </div>
+</footer>
---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,24 @@
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light navbar-static-top">
+    <div class="container">
+        <!-- Branding Image -->
+        <a class="navbar-brand" href="{{ url('/') }}">
+            XgeneBBS
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"  aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <!-- Left side of navbar -->
+            <ul class="navbar-nav mr-auto"></ul>
+
+            <!-- Right side of navbar -->
+            <ul class="navbar-nav navbar-right">
+                <!-- Authentication links -->
+                <li class="nav-item"><a class="nav-link" href="#">登录</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">注册</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>l
