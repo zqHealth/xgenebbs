29a86255b2494d7cea5d0a0290a06b7ff0cad168
zqHealth <zhangqing_health@outlook.com>
2019-07-29 03:55:06 +0000

    裁剪头像

--- app/Handlers/ImageUploadHandler.php
+++ app/Handlers/ImageUploadHandler.php
@@ -2,13 +2,15 @@
 
 namespace App\Handlers;
 
+use function foo\func;
 use Illuminate\Support\Str;
+use Image;
 
 class ImageUploadHandler{
     // 允许上传的文件后缀名
     protected $allowed_ext = ["png", "jpg", "gif", "jpeg"];
 
-    public function save($file, $folder, $file_prefix){
+    public function save($file, $folder, $file_prefix, $max_width=false){
         // 构建存储的文件夹规则 e.g. uploads/images/avatars/201901/10/
         $folder_name = "uploads/images/$folder/" . date("Ym/d", time());
 
@@ -28,8 +30,28 @@ class ImageUploadHandler{
         // 移动图片至目标存储路径
         $file->move($upload_path, $filename);
 
+        // 限制图片宽带，裁剪图片
+        if($max_width && $extension != 'gif'){
+            $this->reduceSize($upload_path . '/' . $filename, $max_width);
+        }
+
         return [
             'path' => config('app.url') . "/$folder_name/$filename"
         ];
+    }
+
+    public function reduceSize($file_path, $max_width){
+        $image = Image::make($file_path);
+
+        // 调整图片大小
+        $image->resize($max_width, null, function ($constraint){
+            // 设定宽度是 $max_width，高度等比例双方缩放
+            $constraint->aspectRatio();
+
+            // 防止裁剪时图片尺寸变大
+            $constraint->upsize();
+        });
+
+        $image->save();
     }
 }
