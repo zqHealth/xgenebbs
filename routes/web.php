2ba1e453d53ca31054cd76bff4ee4cad32ec7eea
zqHealth <zhangqing_health@outlook.com>
2019-07-29 06:48:16 +0000

    生成话题骨架

--- routes/web.php
+++ routes/web.php
@@ -44,3 +44,4 @@ Route::get('email/resend', 'Auth\VerificationController@resend')->name('verifica
 Route::resource('users', 'UsersController', ['only' => [
     'show', 'update', 'edit'
 ]]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
