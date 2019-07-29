1b2c1e86e87590cd6622e4f590e22667b7d1d21a
zqHealth <zhangqing_health@outlook.com>
2019-07-29 06:00:57 +0000

    授权访问

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,15 +9,24 @@ use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
+    // 未登录用户限制： 使用中间件Auth验证用户身份，游客只能看到用户主页
+    public function __construct(){
+        $this->middleware('auth', ['except' => ['show']]);
+    }
+
     public function show(User $user){
         return view('users.show', compact('user'));
     }
 
     public function edit(User $user){
+        // 用户授权策略: App\Policies\UserPolicy
+        $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
 
     public function update(UserRequest $request, User $user, ImageUploadHandler $uploader){
+        // 用户授权策略: App\Policies\UserPolicy
+        $this->authorize('update', $user);
         $data = $request->all();
 
         if($request->avatar){
