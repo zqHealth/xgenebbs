70947aab24846f410c5775e364cb2aa1dc7e7e0e
zqHealth <zhangqing_health@outlook.com>
2019-07-29 08:28:31 +0000

    填充 user 和 topic 数据

--- database/seeds/TopicsTableSeeder.php
+++ database/seeds/TopicsTableSeeder.php
@@ -2,16 +2,31 @@
 
 use Illuminate\Database\Seeder;
 use App\Models\Topic;
+use App\Models\User;
+use App\Models\Category;
 
 class TopicsTableSeeder extends Seeder
 {
     public function run()
     {
-        $topics = factory(Topic::class)->times(50)->make()->each(function ($topic, $index) {
-            if ($index == 0) {
-                // $topic->field = 'value';
+        // 所有用户 ID 数组 e.g. [1, 2, 3, 4]
+        $user_ids = User::all()->pluck('id')->toArray();
+
+        // 所有分类 ID 数组 e.g. [1, 2, 3, 4]
+        $category_ids = Category::all()->pluck('id')->toArray();
+
+        // Faker 实例
+        $faker = app(Faker\Generator::class);
+
+        $topics = factory(Topic::class)
+            ->times(50)
+            ->make()
+            ->each(function ($topic, $index) use ($user_ids, $category_ids, $faker) {
+                // 随机分配一个用户 ID
+                $topic->user_id = $faker->randomElement($user_ids);
+                $topic->category_id = $faker->randomElement($category_ids);
             }
-        });
+        );
 
         Topic::insert($topics->toArray());
     }
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // 获取 Faker 实例
+        $faker = app(Faker\Generator::class);
+
+        // 头像
+        $avatars = [
+            'https://cdn.learnku.com/uploads/images/201710/14/1/s5ehp11z6s.png',
+            'https://cdn.learnku.com/uploads/images/201710/14/1/Lhd1SHqu86.png',
+            'https://cdn.learnku.com/uploads/images/201710/14/1/LOnMrqbHJn.png',
+            'https://cdn.learnku.com/uploads/images/201710/14/1/xAuDMxteQy.png',
+            'https://cdn.learnku.com/uploads/images/201710/14/1/ZqM7iaP4CR.png',
+            'https://cdn.learnku.com/uploads/images/201710/14/1/NDnzMutoxX.png',
+        ];
+
+        // 批量生成数据合集
+        $users = factory(User::class)->times(10)->make()
+            ->each(function ($user, $index) use ($faker, $avatars){
+                $user->avatar = $faker->randomElement($avatars);
+            }
+        );
+
+        // 插入到数据库中
+        $user_array = $users->makeVisible(['password', 'remember_token'])->toArray();
+        User::insert($user_array);
+
+        // 设置第一个用户
+        $user = User::find(1);
+        $user->name = 'zhangqing';
+        $user->email = 'zhangqing_health@outlook.com';
+        $user->avatar = 'https://cdn.learnku.com/uploads/images/201710/14/1/ZqM7iaP4CR.png';
+        $user->save();
+    }
+}
