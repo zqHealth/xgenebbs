48ace1209beca03d449df032e7940a56817758b4
zqHealth <zhangqing_health@outlook.com>
2019-07-31 03:17:38 +0000

    用户可以删除自己发布的话题

--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -44,15 +44,21 @@
             {!! $topic->body !!}
           </div>
 
-          <div class="operate">
-            <hr>
-            <a href="{{ route('topics.edit', $topic->id) }}" class="btn btn-outline-secondary btn-sm" role="button">
-              <i class="far fa-edit"></i> 编辑
-            </a>
-            <a href="#" class="btn btn-outline-secondary btn-sm" role="button">
-              <i class="far fa-trash-alt"></i> 删除
-            </a>
-          </div>
+          @can('update', $topic)
+            <div class="operate">
+              <hr>
+              <a href="{{ route('topics.edit', $topic->id) }}" class="btn btn-outline-secondary btn-sm" role="button">
+                <i class="far fa-edit"></i> 编辑
+              </a>
+              <form action="{{ route('topics.destroy', $topic->id) }}" method="POST" style="display: inline-block;" onsubmit="return confirm('确定删除？');">
+                {{ csrf_field() }}
+                {{ method_field('DELETE') }}
+                <button type="submit" class="btn btn-outline-secondary btn-sm">
+                  <i class="far fa-trash-alt"></i> 删除
+                </button>
+              </form>
+            </div>
+          @endcan
         </div>
       </div>
     </div>
