3d144ddeb64885176c41d5a93f72240dab5110a6
zqHealth <zhangqing_health@outlook.com>
2019-08-02 06:47:38 +0000

    内容管理后台

--- config/administrator.php
+++ config/administrator.php
@@ -43,6 +43,11 @@ return array(
             'roles',
             'permissions',
         ],
+        '内容管理' => [
+            'categories',
+            'topics',
+            'replies',
+        ],
     ],
 
     /*
